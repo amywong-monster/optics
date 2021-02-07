41083cdb3082d095db6f426a77673df0a5589ce1
Amy Wong <amy.wong@monster.com>
2021-02-08 09:48:42 +1100

    Draft

---
+++ src/main/scala/org/jinilover/optics/CityFuncs.scala
@@ -0,0 +1,9 @@
+package org.jinilover.optics
+
+case class City(name: String, area: Double, population: Double)
+
+object CityFuncs {
+  def updatePopulation(city: City): City =
+    city.copy(population = city.population * 1.1)
+    // this is the same as `City(city.name, city.area, city.population * 1.1)`
+}
