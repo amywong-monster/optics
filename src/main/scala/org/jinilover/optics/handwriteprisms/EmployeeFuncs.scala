bb03b508149e9a98870eefb2bfa12a3269d60be3
Amy Wong <amy.wong@monster.com>
2021-02-16 17:03:56 +1100

    Draft prism

---
+++ src/main/scala/org/jinilover/optics/handwriteprisms/EmployeeFuncs.scala
@@ -0,0 +1,38 @@
+package org.jinilover.optics.handwriteprisms
+
+object EmployeeFuncs {
+  import monocle.macros.Lenses
+
+  @Lenses
+  case class Street(number: Int, name: String)
+  // start of Address ADT
+  sealed trait Address
+  @Lenses
+  case class UkAddress(postTown: String, postcode: String, street: Street) extends Address
+  @Lenses
+  case class AusAddress(state: String, postcode: Int, street: Street) extends Address
+  // end of Address ADT
+  @Lenses
+  case class Company(name: String, address: Address)
+  @Lenses
+  case class Employee(name: String, company: Company)
+
+  import monocle.Prism
+  import monocle.macros.GenPrism
+
+  val ukAddressP: Prism[Address, UkAddress] = GenPrism[Address, UkAddress]
+  val ausAddressP: Prism[Address, AusAddress] = GenPrism[Address, AusAddress]
+
+  // try
+  // Employee("john", Company("monster", AusAddress("nsw", 2000, Street(83, "clarence street"))))
+  // Employee("john", Company("monster", UkAddress("london", "w5 5yz", Street(83, "clarence street"))))
+  // in the following functions
+
+  def capitaliseAusState(origEmployee: Employee): Employee =
+    Employee.company.composeLens(Company.address).composePrism(ausAddressP).composeLens(AusAddress.state).modify(_.capitalize)(origEmployee)
+
+  def updateUkStreetNumb(origEmployee: Employee, newStreetNum: Int): Employee =
+    Employee.company.composeLens(Company.address).composePrism(ukAddressP).composeLens(UkAddress.street).composeLens(Street.number).set(newStreetNum)(origEmployee)
+
+
+}
